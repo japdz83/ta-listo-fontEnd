30b47e7d276106168a8240381e84b5b679d268ab
Johan Prado <japdz83@gmail.com>
2020-10-16 09:01:51 -0400

    Se agregaron estilos en la seccion de resultados

--- src/js/views/resultado.js
+++ src/js/views/resultado.js
@@ -2,14 +2,18 @@ import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
 import "../../styles/index.scss";
 import { Navbar } from "../component/navbar";
+import checked from "../../img/checked.png";
 
 export const Resultado = () => {
 	return (
 		<div className="bg text-center">
 			<div className="container p-5">
 				<div className="card mt-5">
+					<div className="text-center p-1 mt-4 logo-home">
+						<img src={checked} />
+					</div>
 					<div className="card-body p-5">
-						<h2 className="card-title">Tú Pedido esta Listo</h2>
+						<h2 className="card-title">Tu pedido esta Listo</h2>
 						<h3 classNames="card-subtitle mb-2 text-muted">Carmen Aguilar</h3>
 						<p className="card-text">Puedes pasar a retirarlo en lo que tengas oportunidad</p>
 					</div>
